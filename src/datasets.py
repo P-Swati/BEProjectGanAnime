04c99f96de6bc4592bbea7cb0ba0116212a9bd32
projectgroup2110 <60421262+projectgroup2110@users.noreply.github.com>
2020-02-24 16:01:13 +0530

    Update datasets.py

--- src/datasets.py
+++ src/datasets.py
@@ -5,15 +5,6 @@ import numpy as np
 import torch
         
 class Anime:
-    """ Dataset that loads images and image tags from given folders.
-
-    Attributes:
-        root_dir: folder containing training images
-        tags_file: a dictionary object that contains class tags of images.
-        transform: torch.Transform() object to perform image transformations.
-        img_files: a list of image file names in root_dir
-        dataset_len: number of training images.
-    """
 
     def __init__(self, root_dir, tags_file, transform):
         with open(tags_file, 'rb') as file:
@@ -27,7 +18,6 @@ class Anime:
         return self.dataset_len
     
     def get_item(self, idx):
-        """ Return '[idx].jpg' and its tags. """
 
         hair_tag, eye_tag = self.tags_file[idx]
         
@@ -40,31 +30,12 @@ class Anime:
         return img, hair_tag, eye_tag
 
 class Shuffler:
-    """ Class that supports andom sampling of training data.
-
-    Attributes:
-        dataset: an Anime dataset object.
-        batch_size: size of each random sample.
-        dataset_len: size of dataset.
-    
-    """
     def __init__(self, dataset, batch_size):
         self.dataset = dataset
         self.batch_size = batch_size
         self.dataset_len = self.dataset.length()
     
     def get_batch(self):
-        """ Returns a batch of randomly sampled images and its tags. 
-
-        Args:
-            None.
-
-        Returns:
-            Tuple of tensors: img_batch, hair_tags, eye_tags
-            img_batch: tensor of shape N * 3 * 64 * 64
-            hair_tags: tensor of shape N * hair_classes
-            eye_tags: tensor of shape N * eye_classes
-        """
 
         indices = np.random.choice(self.dataset_len, self.batch_size)  # Sample non-repeated indices
         img_batch, hair_tags, eye_tags = [], [], []
