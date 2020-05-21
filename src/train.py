09669b3f553b055b0063e589e2396111a3f5d7d8
P-Swati <43266293+P-Swati@users.noreply.github.com>
2020-05-21 23:52:48 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -9,10 +9,13 @@ import os
 
 from datasets import DataLoad, RandomBatchGetter
 from ACGAN import MyConGANGen, MyConDisc
-from utils import save_model, denorm, plot_loss, plot_classifier_loss, show_process
-from utils import generation_by_attributes, get_random_label
+from utils import plot_loss, storeModelParams, denorm
+from utils import generateByHairEye, getRandomLable
 
 device='cuda'
+
+def mul(a,b):
+    return a*b
 
 def main():
     batch_size = 128
@@ -45,8 +48,8 @@ def main():
     G = MyConGANGen(latentVectorSize = latentVecDim, classVectorSize = totalNumOfClasses).to(device)
     
 
-    G_optim = optim.Adam(G.parameters(), betas = [0.5, 0.999], lr = 0.0002)
-    D_optim = optim.Adam(D.parameters(), betas = [0.5, 0.999], lr = 0.0002)
+    optimGen = optim.Adam(G.parameters(), betas = [0.5, 0.999], lr = 0.0002)
+    optimDisc = optim.Adam(D.parameters(), betas = [0.5, 0.999], lr = 0.0002)
     
     d_losses=list()
     g_losses=list()
@@ -60,86 +63,80 @@ def main():
         real_label = torch.ones(batch_size).to(device)
         fake_label = torch.zeros(batch_size).to(device)
         
-        # Train discriminator
-        real_img, hair_tags, eye_tags = RandomBatchGetter.getDataBatch()
-        
-        real_img= real_img.to(device) 
-        hair_tags= hair_tags.to(device)
-        eye_tags=eye_tags.to(device)
-        
-        real_tag = torch.cat((hair_tags, eye_tags), dim = 1)
-        
-        print("actual tag of image"+str(real_tag))
-         
+        ############# Train discriminator ########################################
         #fake batch
         z = torch.randn(batch_size, latentVecDim).to(device)
         
-        fake_tag = get_random_label(batch_size = batch_size, 
+        fakeTag = getRandomLable(batch_size = batch_size, 
                                     hair_classes = hair_classes,
                                     eye_classes = eye_classes).to(device)
         
-        fake_img = G(z, fake_tag).to(device)
+        fakeImage = G(z, fakeTag).to(device)
+        
+        print("real batch")
+        realImage, hairClasses, eyeClasses = RandomBatchGetter.getDataBatch()
+        
+        realImage= realImage.to(device) 
+        hairClasses= hairClasses.to(device)
+        eyeClasses=eyeClasses.to(device)
+        
+        realTag = torch.cat((hairClasses, eyeClasses), dim = 1)
+        
+        print("actual tag of image"+str(realTag))
                 
          #pass through D
-        realProbab, realMultiPredict = D(real_img)
-        fakeProbab, fakeMultiPredict = D(fake_img)
+        realProbab, realMultiPredict = D(realImage)
+        fakeProbab, fakeMultiPredict = D(fakeImage)
             
-        real_discrim_loss = lossFunc(realProbab, real_label)
-        fake_discrim_loss = lossFunc(fakeProbab, fake_label)
+        real_discrim_loss = lossFunc(realProbab, 1)
+        fake_discrim_loss = lossFunc(fakeProbab, 0)
 
-        real_classifier_loss = lossFunc(realMultiPredict, real_tag)
+        realClassifierLoss = lossFunc(realMultiPredict, realTag)
         
-        discrim_loss = (real_discrim_loss + fake_discrim_loss) * 0.5
-        classifier_loss = real_classifier_loss * args.classification_weight
+        totalDiscrimloss = (real_discrim_loss + fake_discrim_loss) * 0.5
+        totalclassificationLayerLoss = mul(realClassifierLoss ,classification_weight)
         
         classifier_log.append(classifier_loss.item())
-            
-        D_loss = discrim_loss + classifier_loss
-        D_optim.zero_grad()
-        D_loss.backward()
-        D_optim.step()
-
+       
         # Train generator
         z = torch.randn(batch_size, latentVecDim).to(device)
-        fake_tag = get_random_label(batch_size = batch_size, 
-                                    hair_classes = hair_classes, hair_prior = hair_prior,
-                                    eye_classes = eye_classes, eye_prior = eye_prior).to(device)
-        fake_img = G(z, fake_tag).to(device)
+        fakeTag = getRandomLable(batch_size = batch_size, hair_classes = hair_classes, eye_classes = eye_classes).to(device)
         
-        fake_score, fake_predict = D(fake_img)
+        fakeImage = G(z, fakeTag).to(device)
+        
+        DLoss = totalDiscrimloss + totalclassificationLayerLoss
+        optimDisc.zero_grad()
+        DLoss.backward()
+        optimDisc.step()
+        
+        fake_score, fake_predict = D(fakeImage)
         
         discrim_loss = lossFunc(fake_score, real_label)
-        classifier_loss = lossFunc(fake_predict, fake_tag)
+        classifier_loss = lossFunc(fake_predict, fakeTag)
         
-        G_loss = classifier_loss + discrim_loss
-        G_optim.zero_grad()
-        G_loss.backward()
-        G_optim.step()
-            
-        ########## Updating logs ##########
-        d_losses.append(D_loss.item())
-        g_losses.append(G_loss.item())
-        show_process(total_steps = iterations, step_i = curIteration,
-        			 g_log = g_losses, d_log = d_losses, classifier_log = classifier_log)
+        GLoss = classifier_loss + discrim_loss
+        optimGen.zero_grad()
+        GLoss.backward()
+        optimGen.step()
 
-        ########## Checkpointing ##########
+        ########## save model configuration ##########
 
         if curIteration == 1:
-            save_image(denorm(real_img[:64,:,:,:]), os.path.join(random_sample_dir, 'real.png'))
+            save_image(denorm(realImage[:64,:,:,:]), os.path.join('../content/saveimage/', 'real.png'))
         if curIteration % 500 == 0:
-            save_image(denorm(fake_img[:64,:,:,:]), os.path.join(random_sample_dir, 'fake_step_{}.png'.format(curIteration)))
+            save_image(denorm(fakeImage[:64,:,:,:]), os.path.join('../content/saveimage/', 'fake{}.png'.format(curIteration)))
             
         if curIteration % 2000 == 0:
-            save_model(model = G, optimizer = G_optim, step = curIteration, log = tuple(g_losses), 
-                       file_path = os.path.join(checkpoint_dir, 'G_{}.ckpt'.format(curIteration)))
-            save_model(model = D, optimizer = D_optim, step = curIteration, log = tuple(d_losses), 
-                       file_path = os.path.join(checkpoint_dir, 'D_{}.ckpt'.format(curIteration)))
+            storeModelParams(modRef = G, optRef = optimGen, step = curIteration, log = tuple(g_losses), 
+                       location = os.path.join('../content/checkpoint', 'G_{}.ckpt'.format(curIteration)))
+            storeModelParams(modRef = D, optRef = optimDisc, step = curIteration, log = tuple(d_losses), 
+                       location = os.path.join('../content/checkpoint', 'D_{}.ckpt'.format(curIteration)))
             
-            plot_loss(g_log = g_losses, d_log = d_losses, file_path = os.path.join(checkpoint_dir, 'loss.png'))
+            plot_loss(g_log = g_losses, d_log = d_losses, file_path = os.path.join('../content/lossplot/', 'loss.png'))
             
-            generation_by_attributes(model = G, device = args.device, step = curIteration, latent_dim = latentVecDim, 
+            generateByHairEye(model = G, device = 'cuda', step = curIteration, latent_dim = latentVecDim, 
                                      hair_classes = hair_classes, eye_classes = eye_classes, 
-                                     sample_dir = fixed_attribute_dir)
+                                     sample_dir = '../content/sample/')
     
 if __name__ == '__main__':
     main()
