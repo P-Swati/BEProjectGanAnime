8058bddbddbbfdd3f93cb6d227c55417a88f20a2
P-Swati <43266293+P-Swati@users.noreply.github.com>
2020-05-21 18:02:02 +0530

    Update ACGAN.py

--- src/ACGAN.py
+++ src/ACGAN.py
@@ -4,78 +4,6 @@ import torch.nn.ConvTranspose2d as CT2d
 import torch.nn.BatchNorm2d as BN2d
 import torch.nn.Conv2d as C2d
 
-class MyConGANGen(nn.Module):
-    def __init__(self, latentVectorSize, classVectorSize):
-	
-        super(MyConGANGen, self).__init__()
-	
-        self.weight=1
-	self.bias=0
-        self.lD = latentVectorSize*self.weight + self.bias
-        self.cD = classVectorSize*self.weight + self.bias
-	
-	def concatenate(a,b):
-		return a+b
-	
-        self.PackedLayersofGen = nn.Sequential(
-			#layer 1
-                    CT2d(in_channels = concatenate(self.lD,self.cD), #concatenate latent and class vec
-                                       out_channels = 1024, 
-                                       kernel_size = 4,
-                                       stride = 1,
-                                       bias = False),
-                    BN2d(1024),
-                    nn.ReLU(inplace = True),
-			#layer 2
-                    CT2d(in_channels = 1024,
-                                       out_channels = 512,
-                                       kernel_size = 4,
-                                       stride = 2,
-                                       padding = 1,
-                                       bias = False),
-                    BN2d(512),
-                    nn.ReLU(inplace = True),
-			#layer 3
-                    CT2d(in_channels = 512,
-                                       out_channels = 256,
-                                       kernel_size = 4,
-                                       stride = 2,
-                                       padding = 1,
-                                       bias = False),
-                    BN2d(256),
-                    nn.ReLU(inplace = True),
-			#layer 4
-                    CT2d(in_channels = 256,
-                                       out_channels = 128,
-                                       kernel_size = 4,
-                                       stride = 2,
-                                       padding = 1,
-                                       bias = False),
-                    BN2d(128),
-                    nn.ReLU(inplace = True),
-			#layer 5
-                    CT2d(in_channels = 128,
-                                       out_channels = 3,
-                                       kernel_size = 4,
-                                       stride = 2,
-                                       padding = 1),
-                    nn.Tanh()
-                    )
-        return
-    
-    def forward(self, ipVec, classVec):
-	print("inside forward function")
-        finalVec = torch.cat((ipVec, classVec), dim = 1)  # Concatenate noise and class vector.
-	print("final appended vector to be fed =")
-	print(finalVec)
-        finalVec = finalVec.unsqueeze(2).unsqueeze(3)  # 2 for dimesions, 3 for color channels
-	print("resized final vector =")
-	print(finalVec)
-	print("pass this to gen input layer and return obtained output")
-        temp=self.PackedLayersofGen(finalVec)
-	print(temp)
-	return temp
-
 class MyConDisc(nn.Module):
     def __init__(self, countOfClasses):
         super(MyConDisc, self).__init__()
@@ -159,18 +87,93 @@ class MyConDisc(nn.Module):
         multiLabOutput = self.multilableClassificationLayer(flatten)
         return realOrFake, multiLabOutput
 
+
+class MyConGANGen(nn.Module):
+    def __init__(self, latentVectorSize, classVectorSize):
+	
+        super(MyConGANGen, self).__init__()
+	
+        self.weight=1
+	self.bias=0
+        self.lD = latentVectorSize*self.weight + self.bias
+        self.cD = classVectorSize*self.weight + self.bias
+	
+	def concatenate(a,b):
+		return a+b
+	
+        self.PackedLayersofGen = nn.Sequential(
+			#layer 1
+                    CT2d(in_channels = concatenate(self.lD,self.cD), #concatenate latent and class vec
+                                       out_channels = 1024, 
+                                       kernel_size = 4,
+                                       stride = 1,
+                                       bias = False),
+                    BN2d(1024),
+                    nn.ReLU(inplace = True),
+			#layer 2
+                    CT2d(in_channels = 1024,
+                                       out_channels = 512,
+                                       kernel_size = 4,
+                                       stride = 2,
+                                       padding = 1,
+                                       bias = False),
+                    BN2d(512),
+                    nn.ReLU(inplace = True),
+			#layer 3
+                    CT2d(in_channels = 512,
+                                       out_channels = 256,
+                                       kernel_size = 4,
+                                       stride = 2,
+                                       padding = 1,
+                                       bias = False),
+                    BN2d(256),
+                    nn.ReLU(inplace = True),
+			#layer 4
+                    CT2d(in_channels = 256,
+                                       out_channels = 128,
+                                       kernel_size = 4,
+                                       stride = 2,
+                                       padding = 1,
+                                       bias = False),
+                    BN2d(128),
+                    nn.ReLU(inplace = True),
+			#layer 5
+                    CT2d(in_channels = 128,
+                                       out_channels = 3,
+                                       kernel_size = 4,
+                                       stride = 2,
+                                       padding = 1),
+                    nn.Tanh()
+                    )
+        return
+    
+    def forward(self, ipVec, classVec):
+	print("inside forward function")
+        finalVec = torch.cat((ipVec, classVec), dim = 1)  # Concatenate noise and class vector.
+	print("final appended vector to be fed =")
+	print(finalVec)
+        finalVec = finalVec.unsqueeze(2).unsqueeze(3)  # 2 for dimesions, 3 for color channels
+	print("resized final vector =")
+	print(finalVec)
+	print("pass this to gen input layer and return obtained output")
+        temp=self.PackedLayersofGen(finalVec)
+	print(temp)
+	return temp
+
+
 if __name__ == '__main__':
-    latentVectorSize = 100
-    classVectorSize = 22
-    batch = 5
-    lVec = torch.randn(batch, latentVectorSize)
-    cVec = torch.randn(batch, classVectorSize)
+    print("latentVector Size = 100")
+    print("classVector Size = 22")
+    print("batch size= 5")
+    
+    lVec = torch.randn(5, 100)
+    cVec = torch.randn(5, 22)
     print("random vector:")
     print(lVec)
     print("class vector")
     print(cVec)
-    GenObject = MyConGANGen(latentVectorSize, classVectorSize)
-    DiscObject = MyConDisc(classVectorSize)
+    GenObject = MyConGANGen(100, 22)
+    DiscObject = MyConDisc(22)
     o = GenObject(lVec, cVec)
 #     print(o.shape)
     x, y = DiscObject(o)
