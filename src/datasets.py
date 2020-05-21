702230b5928e4de73962b0eaee8d507fed539cea
P-Swati <43266293+P-Swati@users.noreply.github.com>
2020-05-21 16:44:23 +0530

    Update datasets.py

--- src/datasets.py
+++ src/datasets.py
@@ -1,52 +1,54 @@
+import torch
+import pickle
 import os
 import cv2
-import pickle
 import numpy as np
-import torch
-        
-class Anime:
 
-    def __init__(self, root_dir, tags_file, transform):
-        with open(tags_file, 'rb') as file:
-            self.tags_file = pickle.load(file) 
-        self.root_dir = root_dir
-        self.transform = transform
-        self.img_files = os.listdir(self.root_dir)
-        self.dataset_len = len(self.img_files)
+        
+class DataLoad:
+
+    def __init__(self, root, tagsPickle, transFunc):
+        with open(tagsPickle, 'rb') as file:
+            self.tagsPickle = pickle.load(file) 
+        self.root = root
+        self.transFunc = transFunc
+        self.images = os.listdir(self.root)
+        self.lengthOfDataset = len(self.images)
+        self.fileType='.jpg'
     
     def length(self):
-        return self.dataset_len
+        return self.lengthOfDataset
     
-    def get_item(self, idx):
+    def getTuple(self, row):
 
-        hair_tag, eye_tag = self.tags_file[idx]
+        hairClass, eyeClass = self.tags_file[row]
         
-        img_path = os.path.join(self.root_dir, str(idx) + '.jpg')
-        img = cv2.imread(img_path)
-        img = img[:, :, (2, 1, 0)]  # Swap B,R channel of np.array loaded with cv2
-                    						 # (BGR -> RGB)
-        if self.transform:
-            img = self.transform(img)
-        return img, hair_tag, eye_tag
+        ipath = os.path.join(self.root, str(row) + self.fileType)
+        pic = cv2.imread(ipath)
+        # (BGR -> RGB)
+        pic = pic[:, :, (2, 1, 0)]  
+            						 
+        if self.transFunc:
+            finalPic = self.transform(pic)
+        return finalPic, hairClass, eyeClass
 
-class Shuffler:
-    def __init__(self, dataset, batch_size):
-        self.dataset = dataset
-        self.batch_size = batch_size
-        self.dataset_len = self.dataset.length()
+class RandomBatchGetter:
+    def __init__(self, data, batch):
+        self.data = data
+        self.bSize = batch
+        self.lenOfDataset = self.data.length()
     
-    def get_batch(self):
-
-        indices = np.random.choice(self.dataset_len, self.batch_size)  # Sample non-repeated indices
-        img_batch, hair_tags, eye_tags = [], [], []
-        for i in indices:
-            img, hair_tag, eye_tag = self.dataset.get_item(i)
-            img_batch.append(img.unsqueeze(0))
-            hair_tags.append(hair_tag.unsqueeze(0))
-            eye_tags.append(eye_tag.unsqueeze(0))
-        img_batch = torch.cat(img_batch, 0)
-        hair_tags = torch.cat(hair_tags, 0)
-        eye_tags = torch.cat(eye_tags, 0)
+    def getDataBatch(self):
+        indexList = np.random.choice(self.lenOfDataset, self.bSize)
+        imageBatch, hairClassVec, eyeClassVec = [], [], []
+        for idx in indexList:
+            img, hairClass, eyeClass = self.data.getTuple(idx)
+            imageBatch.append(img.unsqueeze(0))
+            hairClassVec.append(hairClass.unsqueeze(0))
+            eyeClassVec.append(eyeClass.unsqueeze(0))
+        imageBatch = torch.cat(imageBatch, 0)
+        hairClassVec = torch.cat(hairClassVec, 0)
+        eyeClassVec = torch.cat(eyeClassVec, 0)
         
-        return img_batch, hair_tags, eye_tags
+        return imageBatch, hairClassVec, eyeClassVec
     
