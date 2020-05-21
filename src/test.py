e7dc1e08333791d1331a4445db3e83b0cc6c2f46
P-Swati <43266293+P-Swati@users.noreply.github.com>
2020-05-21 20:19:34 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -13,38 +13,19 @@ import datasets
 import ACGAN
 import anvil.media
 
-hair_mapping =  ['orange', 'white','aqua', 'gray', 'green', 'red', 'purple', 
-                 'pink', 'blue', 'black', 'brown','blonde']
-eye_mapping = ['black', 'orange', 'pink', 'yellow', 'aqua', 'purple', 'green', 
-               'brown', 'red', 'blue']
+hairClassesList =  ['pink', 'blue','orange',  'white','aqua', 'gray','purple', 
+                  'black', 'green', 'red','blonde','brown']
+eyeClassesList= ['pink','red','black', 'orange' , 'purple', 'yellow', 'aqua', 'green', 
+               'brown', 'blue']
 
-hair_dict = {
-    'orange' : 0,
-    'white': 1, 
-    'aqua': 2,
-    'gray': 3,
-    'green': 4,
-    'red': 5,
-    'purple': 6,
-    'pink': 7,
-    'blue': 8,
-    'black': 9,
-    'brown': 10,
-    'blonde': 11
-}
+hair_dict={}
+for i in range(len(hairClassesList)):
+    hair_dict[hairClassesList[i]]=i
 
-eye_dict = {
-    'black': 0,
-    'orange': 1,
-    'pink': 2,
-    'yellow': 3,
-    'aqua' : 4,
-    'purple': 5,
-    'green': 6,
-    'brown': 7,
-    'red': 8,
-    'blue': 9
-}
+eye_dict = {}
+for i in range(len(eyeClassesList)):
+    eye_dict[eyeClassesList[i]]=i
+
 
 def generateUsingHairEye(model, device, hair_classes, eye_classes, lDim, hColor, eColor):
     htag = torch.zeros(64, hair_classes).to(device)
@@ -58,8 +39,8 @@ def generateUsingHairEye(model, device, hair_classes, eye_classes, lDim, hColor,
     fulltag = torch.cat((htag, etag), 1)
     z = torch.randn(64, lDim).to(device)
     
-    output = model(z, tag)
-    save_image(utils.denorm(output), '../generated/{} hair {} eyes.png'.format(hair_mapping[hairLabelIndex], eye_mapping[eyeLabelIndex]))
+    output = model(z, fulltag)
+    save_image(utils.denorm(output), '../generated/{} hair {} eyes.png'.format(hairClassesList[hairLabelIndex], eyeClassesList[eyeLabelIndex]))
 
 def add(hair,eye):
     return hair+eye
@@ -74,15 +55,15 @@ def main(hairColor,eyeColor):
     totalClasses = add(hairClasses,eyeClasses)
     latentDim = 100
     
-    G_path = '/content/BeProjectGANAnime/src/mymodel/ACGAN_generator.ckpt'
+    pathToModel = '/content/BeProjectGANAnime/src/mymodel/ACGAN_generator.ckpt'
 
-    G = ACGAN.MyConGANGen(latentVectorSize = latent_dim, classVectorSize = totalClasses)
-    previouslyTrainedstate = torch.load(G_path)
+    Gen = ACGAN.MyConGANGen(latentVectorSize = latent_dim, classVectorSize = totalClasses)
+    previouslyTrainedstate = torch.load(pathToModel)
     
     print("load state info..")
-    G.load_state_dict(previouslyTrainedstate['model'])
-    G = G.eval()
+    Gen.load_state_dict(previouslyTrainedstate['model'])
+    Gen = Gen.eval()
 
-    generateUsingHairEye(G, 'cpu',hairClasses, eyeClasses, latentDim,hairColor, eyeColor)
+    generateUsingHairEye(Gen, 'cpu',hairClasses, eyeClasses, latentDim,hairColor, eyeColor)
 
     return(anvil.media.from_file('/content/BeProjectGANAnime/generated/{} hair {} eyes.png'.format(hair,eye)))
